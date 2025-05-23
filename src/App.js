de7cb3f75097ba8690b6173cb70023e28a3e12ac
Koh Jhin Kiat <91869260+31-KohJhinKiat@users.noreply.github.com>
2025-05-23 16:48:12 +0800

    need help

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
-import Home from './pages/Home';
-import DesignSkimboard from './pages/DesignSkimboard';
-import Search from './pages/Search';
-import Navbar from './components/Navbar';
+import Home from '/pages/Home';
+import DesignSkimboard from '/pages/DesignSkimboard';
+import Search from '/pages/Search';
+import Navbar from '/components/Navbar';
 
 const App = () => {
   return (
