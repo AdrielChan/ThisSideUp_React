2920486845e6996bbfcbe725695560eb32e497d8
SetaKaoru <rhysdarys@gmail.com>
2025-05-28 08:35:08 +0000

    com

--- src/App.js
+++ src/App.js
@@ -1,73 +1,31 @@
-// File: src/App.js
-import React, { Suspense, lazy } from 'react';
-import { Routes, Route, Navigate } from 'react-router-dom';
 
-// Protected Route Component
-const ProtectedRoute = ({ children }) => {
-  const { isAuthenticated, loading } = useAuth();
-  if (loading) return <LoadingSpinner />; // Or some other loading indicator
-  return isAuthenticated ? children : <Navigate to="/login" replace />;
-};
+// DON'T FORGET, IN YOUR 🫵 TERMINAL:  npm install react-router-dom
+//               AND                    npm i styled-components
 
+import React from 'react';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
+import Home from './pages/Home';
+import DesignSkimboard from './pages/DesignSkimboard';
+import Search from './pages/Search';
+import About from './pages/About';
+import Products from './pages/Products';
+import FAQ from './pages/FAQ';
+import Navbar from './components/Navbar';
 
-function AppContent() {
+const App = () => {
   return (
     <>
       <Navbar />
-      <PageWrapper>
-        <Suspense fallback={<LoadingSpinner />}>
-          <Routes>
-            {/* Public Routes */}
-            <Route path="/" element={<HomePage />} />
-            <Route path="/login" element={<LoginPage />} />
-            <Route path="/signup" element={<SignupPage />} />
-            <Route path="/about" element={<AboutPage />} />
-            <Route path="/faq" element={<FAQPage />} />
-            
-            <Route path="/products" element={<ProductsPage />} />
-            <Route path="/products/category/:categoryName" element={<ProductsPage />} />
-            <Route path="/product/:productId" element={<ProductDetailPage />} />
-            <Route path="/search" element={<SearchResultsPage />} /> 
-            
-            <Route path="/design-skimboard" element={<DesignSkimboardPage />} />
-            <Route path="/cart" element={<CartPage />} />
-
-            {/* Protected Routes */}
-            <Route 
-              path="/checkout" 
-              element={<ProtectedRoute><CheckoutPage /></ProtectedRoute>} 
-            />
-            <Route 
-              path="/order-status" 
-              element={<ProtectedRoute><OrderStatusPage /></ProtectedRoute>} 
-            />
-             <Route 
-              path="/order-confirmation/:orderId" 
-              element={<ProtectedRoute><OrderConfirmationPage /></ProtectedRoute>} 
-            />
-
-            {/* Catch-all */}
-            <Route path="*" element={<NotFoundPage />} />
-          </Routes>
-        </Suspense>
-      </PageWrapper>
-      <Footer />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/design" element={<DesignSkimboard />} />
+        <Route path="/search" element={<Search />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/products" element={<Products />} />
+        <Route path="/faq" element={<FAQ />} />
+      </Routes>
     </>
   );
-}
-
-function App() {
-  return (
-    <AuthProvider>
-      <ProductProvider>
-        <CartProvider>
-          <DesignProvider> {/* DesignContext wraps routes that might use it */}
-            <AppContent />
-          </DesignProvider>
-        </CartProvider>
-      </ProductProvider>
-    </AuthProvider>
-  );
-}
+};
 
 export default App;
