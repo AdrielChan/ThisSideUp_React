c5ef95c1439dd77d50a32607b3965388057ec32c
SetaKaoru <rhysdarys@gmail.com>
2025-05-29 11:15:48 +0000

    pictuf

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import {React} from 'react';
 import styled from 'styled-components';
 
 const Wrapper = styled.div`
@@ -6,10 +6,22 @@ const Wrapper = styled.div`
 `;
 
 const Home = () => {
+  const backgroundImage = "homebanner.png";
+
   return (
+    
     <Wrapper>
-      <h1>Welcome to This Side Up</h1>
-      <p>Explore our skimboards, design your own, or search products!</p>
+      <div style={{
+      backgroundImage: `url(${backgroundImage})`,
+      backgroundSize: 'cover',
+      backgroundRepeat: 'no-repeat',
+      backgroundPosition: 'center',
+      height: '100vh',
+    }}>
+        <h1>Welcome to This Side Up</h1>
+        <p>Explore our skimboards, design your own, or search products!</p>
+      </div>
+      
     </Wrapper>
   );
 };
