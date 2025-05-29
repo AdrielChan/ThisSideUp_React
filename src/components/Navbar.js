f7af88e0b037dd158232680a167936dd5765b038
Keiko <88486200+4DR13LCH4N@users.noreply.github.com>
2025-05-29 18:57:49 +0800

    navigationSpacing

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 import styled from 'styled-components';
 
 const Nav = styled.nav`
@@ -7,27 +7,67 @@ const Nav = styled.nav`
   color: white;
   padding: 1rem;
   display: flex;
-  gap: 1rem;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+const NavLinks = styled.div`
+  display: flex;
+  gap: 2rem;
+  padding-right: 70px;
 `;
 
 const StyledLink = styled(Link)`
   color: white;
   text-decoration: none;
+  font-size: 1.1rem;
+  transition: color 0.3s ease;
+  
   &:hover {
-    text-decoration: underline;
+    color: #b19cd9;
+  }
+
+  &.active {
+    color: #b19cd9;
+    font-weight: bold;
   }
 `;
 
-const Navbar = () => (
-  <Nav style={{alignItems:'center'}}>
-    <StyledLink to="/"><img src="this-side-up-logo_white.png" alt="This Side Up logo" style={{padding: '13px 20px 13px 70px'}} width="290px"/></StyledLink>
-    <div >
-      <StyledLink to="/products">Producks</StyledLink>
-      <StyledLink to="/about">About</StyledLink>
-      <StyledLink to="/faq">FAQ</StyledLink>
-    </div>
-    
-  </Nav>
-);
+const Navbar = () => {
+  const location = useLocation();
+  
+  return (
+    <Nav>
+      <StyledLink to="/">
+        <img 
+          src="this-side-up-logo_white.png" 
+          alt="This Side Up logo" 
+          style={{padding: '13px 20px 13px 70px'}} 
+          width="290px"
+        />
+      </StyledLink>
+      <NavLinks>
+        <StyledLink 
+          to="/products" 
+          className={location.pathname === '/products' ? 'active' : ''}
+        >
+          Products
+        </StyledLink>
+        <StyledLink 
+          to="/about" 
+          className={location.pathname === '/about' ? 'active' : ''}
+        >
+          About
+        </StyledLink>
+        <StyledLink 
+          to="/faq" 
+          className={location.pathname === '/faq' ? 'active' : ''}
+        >
+          FAQ
+        </StyledLink>
+      </NavLinks>
+    </Nav>
+  );
+};
 
 export default Navbar;
