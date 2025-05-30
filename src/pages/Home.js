6782d2fe3609c2677b9d5d3214e7136d7c7dfa12
SetaKaoru <rhysdarys@gmail.com>
2025-05-30 07:48:31 +0000

    Home but better

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,6 @@
 import {React} from 'react';
 import styled from 'styled-components';
-
+import '../index.css';
 const Wrapper = styled.div`
   padding: 0rem;
 `;
@@ -19,10 +19,10 @@ const Home = () => {
       height: '100vh',
       padding: '0rem'
     }}>
-      <div style={{padding: '2rem', backgroundColor: '#202020', opacity: '60%'}}>
-        <h1 style={{fontSize: 36, color: '#FDDDFD'}}>Skim & Ride</h1>
-        <p style={{fontSize: 64, color: '#FFFFFF'}}>Find your next adventure</p>
-        <h1 style={{fontSize: 24, color: '#FDDDFD'}}>Premium skimboards and beach gear, crafted for wave chasers</h1>
+      <div id='front' style={{padding: '2rem', backgroundColor: 'rgba(32, 32, 32, 0.6)', width: '560px'}}>
+        <p style={{fontSize: 36, color: '#FDDDFD'}}>Skim & Ride</p>
+        <p style={{fontSize: 64, color: '#FFFFFF'}}>Find Your Next Adventure</p>
+        <p style={{fontSize: 24, color: '#FDDDFD'}}>Premium skimboards and beach gear, crafted for wave chasers</p>
       </div>
       </div>
       
