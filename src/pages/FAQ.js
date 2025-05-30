740ac427c268bc1f6532b1fca744b8b8632bbdd6
SetaKaoru <rhysdarys@gmail.com>
2025-05-30 10:32:28 +0000

    FAQQQ

--- src/pages/FAQ.js
+++ src/pages/FAQ.js
@@ -2,14 +2,26 @@ import React from 'react';
 import styled from 'styled-components';
 
 const Wrapper = styled.div`
-  padding: 2rem;
+  padding: 0rem;
 `;
 
 const FAQ = () => {
+  const backgroundImage = "homebanner.png";
   return (
     <Wrapper>
-      <h1>Frequently Asked Questions</h1>
-      <p>Explore our skimboards, design your own, or search products!</p>
+      <div style={{
+      backgroundImage: `url(${backgroundImage})`,
+      backgroundSize:'1950px',
+      backgroundRepeat: 'no-repeat',
+      backgroundPosition: 'center',
+      height: '100vh',
+      padding: '0rem'
+    }}>
+      <div id='font1' style={{padding: '2rem', backgroundColor: 'rgba(32, 32, 32, 0.6)', width: '650px', paddingLeft: '30px', position: 'relative', left: '110px'}}>
+        <p style={{fontSize: 64, color: '#FFFFFF', lineHeight: '63px', paddingBottom: '20px', textAlign: 'center'}}>Frequently Asked Questions (FAQs) </p>
+      </div>
+      </div>
+      
     </Wrapper>
   );
 };
