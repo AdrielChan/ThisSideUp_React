73c5d292145a8b69e71bcf98e1460dfd8ed74825
SetaKaoru <rhysdarys@gmail.com>
2025-06-01 06:20:29 +0000

    questions

--- src/pages/FAQ.js
+++ src/pages/FAQ.js
@@ -20,7 +20,19 @@ const FAQ = () => {
       <div id='font1' style={{paddingTop: '15px',backgroundImage: 'linear-gradient(to right, rgba(27,0,39,0.6), rgba(96,0,141,0.6), rgba(96,0,141,0.6), rgba(27,0,39,0.6))', width: '650px', height: '1000px', paddingLeft: '30px', position: 'relative', margin: 'auto', }}>
         <p style={{fontSize: 64, color: '#FFFFFF', lineHeight: '63px', paddingBottom: '20px', textAlign: 'center', textShadow: '2px 2px 5px rgba(0,0,0,0.25)'}}>Frequently Asked Questions (FAQs)</p>
         <div id='qn'>
-          <p>What the hell?</p>
+          <p>How do I customise my skim board?</p>
+        </div>
+        <div id='qn'>
+          <p>How long does order processing take?</p>
+        </div>
+        <div id='qn'>
+          <p>What if my order arrives damaged?</p>
+        </div>
+        <div id='qn'>
+          <p>Do we ship internationally?</p>
+        </div>
+        <div id='qn'>
+          <p>Do we accept returns/refunds?</p>
         </div>
       </div>
       </div>
