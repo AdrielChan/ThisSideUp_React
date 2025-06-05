21e2a4586067158c9798d6e95450335fee971e95
Keiko <88486200+4DR13LCH4N@users.noreply.github.com>
2025-06-05 11:01:16 +0000

    shadow

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -44,10 +44,10 @@ const Home = () => {
       height: '100vh',
       padding: '0rem'
     }}>
-      <div id='font1' style={{padding: '2rem', backgroundColor: 'rgba(32, 32, 32, 0.6)', width: '560px', paddingLeft: '30px', position: 'relative', left: '110px', textShadow: '2px 2px 5px rgba(0,0,0,0.25)'}}>
-        <p style={{fontSize: 36, color: '#FDDDFD', textShadow: '2px 2px 5px rgba(0,0,0,0.25)'}}>Skim & Ride</p>
+      <div id='font1' style={{padding: '2rem', backgroundColor: 'rgba(32, 32, 32, 0.6)', width: '560px', paddingLeft: '30px', position: 'relative', left: '110px', textShadow: '2px 2px 5px rgba(0,0,0,0.75)'}}>
+        <p style={{fontSize: 36, color: '#FE9C7F', textShadow: '2px 2px 5px rgba(0,0,0,0.7)'}}>Skim & Ride</p>
         <p style={{fontSize: 64, color: '#FFFFFF', lineHeight: '63px', paddingBottom: '20px'}}>Find Your Next Adventure</p>
-        <p style={{fontSize: 24, color: '#FDDDFD', lineHeight: '33px'}}>Premium skimboards and beach gear, crafted for wave chasers</p>
+        <p style={{fontSize: 24, color: '#FE9C7F', lineHeight: '33px'}}>Premium skimboards and beach gear, crafted for wave chasers</p>
         <p id='longpara' style={{fontSize: 20, color: '#FFFFFF'}}>
           This Side Up is an online store for skimboards, beach
           supplies, and custom board designs. The website
